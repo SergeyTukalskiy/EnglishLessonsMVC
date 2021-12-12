302dbb188700225df2d330063098db79c20696a5
SergeyTukalskiy <sergeytukalsky@gmail.com>
2021-12-12 13:37:34 +0300

    Добавьте файлы проекта.

---
+++ EnglishLessonsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnglishLessonsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
